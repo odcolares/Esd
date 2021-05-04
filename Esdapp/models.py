9025b5168df225c9f81310423d254f19ba389ff7
odcolares <71576484+odcolares@users.noreply.github.com>
2021-05-04 16:35:19 -0400

    Segundo commit

--- Esdapp/models.py
+++ Esdapp/models.py
@@ -5,3 +5,4 @@ class esd(models.Model):
     nome = models.CharField(max_length=100)
     setor = models.CharField(max_length=100)
     matricula = models.IntegerField()
+    status = models.IntegerField()
